1e3466614256e103000a42ab1fe9c4808fe3fe39
David <gamesdevdave@gmail.com>
2020-02-29 03:58:14 +0000

    - Added Anchoring to the game.  - Implemented constant movement when not anchored.

---
+++ PirateGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    float playerSpeed = 2f;
+    Rigidbody rb;
+    bool anchorDown = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Anchor();
+
+        if (anchorDown == false)
+        {
+            rb.velocity = transform.forward * playerSpeed;
+        }
+    }
+
+    private void Anchor()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && anchorDown == false)
+        {
+            anchorDown = true;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F) && anchorDown == true)
+        {
+            anchorDown = false;
+        }
+    }
+}
