ecf227636e3e16561517d77d3e99c967a103ab39
DAVID BROWN <DB228174@falmouth.ac.uk>
2020-02-29 19:17:08 +0000

    - Cleaned up scripts a bit.  - Made it so it now stops and changes camera.

---
+++ PirateGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    float playerSpeed = 2f;
+    Rigidbody rb;
+    [SerializeField]
+    float turnSpeed = 0.5f;
+    public bool anchorDown = false;
+    public GameObject mainCamera;
+    public GameObject sideCamera;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Anchor();
+
+        if (anchorDown == false)
+        {
+            rb.velocity = transform.forward * playerSpeed;
+        }
+
+        Turning();
+    }
+
+    private void Anchor()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && anchorDown == false)
+        {
+            mainCamera.SetActive(false);
+            sideCamera.SetActive(true);
+            anchorDown = true;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.F) && anchorDown == true)
+        {
+            sideCamera.SetActive(false);
+            mainCamera.SetActive(true);
+            anchorDown = false;
+        }
+    }
+
+    private void Turning()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, turnSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -turnSpeed, 0);
+        }
+    }
+}
