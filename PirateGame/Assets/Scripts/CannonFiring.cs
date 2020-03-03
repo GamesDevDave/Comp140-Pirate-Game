6d53c06a0a7bbbd8522752a6debb3e4ce2153c52
David <gamesdevdave@gmail.com>
2020-03-03 10:54:29 +0000

    - Added script for turning the cannon.

---
+++ PirateGame/Assets/Scripts/CannonFiring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonFiring : MonoBehaviour
+{
+    [SerializeField]
+    float maxRotation = -120f;
+    [SerializeField]
+    float minRotation = -60f;
+    [SerializeField]
+    float rotationSpeed;
+
+    bool hasFired = true;
+
+    public GameObject cannon;
+
+    private void Update()
+    {
+        DetectInput();
+    }
+
+    private void DetectInput()
+    {
+        cannon.transform.Rotate(0, Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime, 0, Space.World);
+    }
+}
