1e4988877c6d6eaf341004d435510cbfb3d53613
DB228174 <DB228174@falmouth.ac.uk>
2020-03-09 12:17:11 +0000

    - Added arduino script.

---
+++ PirateGame/Assets/Scripts/ArduinoInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using System.IO.Ports;
+public class ArduinoInput : MonoBehaviour
+{
+    public int commPort = 6;
+    public int steerValue;
+
+    private SerialPort serial;
+
+    // Use this for initialization
+    void Start()
+    {
+        ConnectToSerial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        serial.ReadLine();
+    }
+
+    void ConnectToSerial()
+        {
+            Debug.Log("Attempting Serial: " + commPort);
+
+            serial = new SerialPort("\\\\.\\COM" + commPort, 9600);
+            serial.ReadTimeout = 50;
+            serial.Open();
+        }
+}
+    
